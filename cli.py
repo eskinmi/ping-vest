bc1bea170eeb57777959972c318cccc83a43a0ee
mustafasamedeskin <meskin@wehkamp.nl>
2021-12-08 22:18:04 +0300

    modularize the push

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ import detect
 @click.option("--query", default='ASML')
 @click.option("--interval", default='60min')
 def cli(topic, query, interval):
-    detect.serve_unit(topic, query, interval)
+    detect.detect(topic, query, interval)
 
 
 if __name__ == "__main__":
--- detect.py
+++ detect.py
@@ -1,20 +1,24 @@
 from pingvest.pipe import *
 
 
-def serve_unit(topic, query, interval):
-    if topic == 'fx':
-        exc = ForeignExchange(query, interval)
-    elif topic == 'sx':
-        exc = StockExchange(query, interval)
-    else:
-        raise ValueError('topic should be one of the following : sx, fx')
-    exc.get()
-    series = exc.process()
-    outliers = stats.outliers(series, 3)
-    # FIND DATES OF OUTLIERS
-    if outliers:
-        notify(
-            title=F"{topic.upper()} Exchange Alert! {query}",
-            text=F"high fluctuations in value at : {' '.join(str(exc.keys[i]) for i in outliers)}"
+class NaiveDetector:
+
+    def __init__(self, collector):
+        self.collector = collector
+        self.collector.get()
+
+    def detect(self):
+        return stats.outliers(
+            self.collector.process()
         )
 
+
+def detect(topic, query, interval):
+    exc = StockExchange(query=query, interval=interval)
+    det = NaiveDetector(collector=exc)
+    alerts = det.detect()
+    if alerts:
+        notify(
+            title=F"{topic.upper()} Exchange Alert! {query}",
+            text=F"high fluctuations in value at : {' '.join(str(exc.keys[i]) for i in alerts)}"
+        )
